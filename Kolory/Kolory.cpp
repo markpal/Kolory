d6dfda561e0c4dd74e148209e713401304a9fff5
Marek Pałkowski <mpalkowski@zut.edu.pl>
2023-03-17 11:41:04 +0100

    kolo

--- Kolory/Kolory.cpp
+++ Kolory/Kolory.cpp
@@ -28,19 +28,23 @@ void textColor(char c = 'W')
 
     switch (c) {
     case 'Y':   //yellow
-            colors = FOREGROUND_RED | FOREGROUND_GREEN;
-            break;
-    case 'R': 
-            colors = 12;
-            break;
-    case 'G': 
-            colors = FOREGROUND_GREEN;
-            break;
-    case 'B': 
+        colors = FOREGROUND_RED | FOREGROUND_GREEN;
+        break;
+    case 'R':
+        colors = 12;
+        break;
+    case 'G':
+        colors = FOREGROUND_GREEN;
+        break;
+    case 'B':
         colors = FOREGROUND_BLUE | FOREGROUND_GREEN;
         break;
-    case 'P': 
+    case 'P':
         colors = FOREGROUND_RED | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
+        break;
+    default:
+        //colors = 22;
+        break;
     }
 
     SetConsoleTextAttribute(hStdOut, colors);
@@ -52,7 +56,7 @@ void textPlayers()
     int i;
     textColor();
     cout << "\n";
-    for(i=0; i<3; i++)
+    for (i = 0; i < 3; i++)
         cout << gracze[i].imie << "\t" << gracze[i].kasa << "\n";
     cout << "\n";
 }
@@ -60,9 +64,58 @@ void textPlayers()
 
 int main()
 {
+    int i;
+
+    string haslo = "wielka antarktyda";
+    char literka;
+    int n = haslo.size();
+    int maska[100];
+    int suma = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        if (haslo[i] == ' ')
+            maska[i] = 0;
+        else
+            maska[i] = 1;
+    }
+
+    do
+    {
+        cout << "Podaj litere" << endl;
+        cin >> literka;
+
+        for (i = 0; i < n; i++)
+        {
+            if (haslo[i] == literka)
+                maska[i] = 0;
+        }
+
+        textColor('P');
+        for (i = 0; i < n; i++)
+        {
+
+            if (maska[i] == 1)
+                cout << ".";
+            else
+                cout << haslo[i];
+        }
+        textColor();
+        cout << endl;
+
+        suma = 0;
+
+        for (i = 0; i < n; i++)
+        {
+            suma += maska[i];
+        }
+
+    } while (suma);
+    cout << "BRAWO!";
+    exit(0);
     /* initialize random seed: */
     srand(time(NULL));
-    
+
     textColor('P');
 
     //char rainbow[] = { 'R','G','Y','B'};
@@ -72,7 +125,8 @@ int main()
     //system("cls");
     textColor();
 
-    cout << rand() % 10 + 1;
+    i = rand() % 15;
+    cout << Kolo[i];
 
     gracze[0].imie = "Monika";
     gracze[1].imie = "Jan";
@@ -81,4 +135,3 @@ int main()
     textPlayers();
     Beep(523, 500);
 }
-
